46ec4cd1693d724b8845ebc854efcf84953e88ad
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-26 20:40:12 -0300

    teminei

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double glicemia;
+ 
+Console.Write("Digite o Valor de sua Glicemia: ");
+glicemia = Convert.ToDouble(Console.ReadLine());
+ 
+if( glicemia < 92 )
+{
+    Console.WriteLine("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose");
+}
+else
+{
+    if( glicemia >= Convert.ToDouble(92) && glicemia < Convert.ToDouble(126) )
+    {
+        Console.WriteLine("Compatível com Diabetes Gestacional");
+    }
+    else if( glicemia >= Convert.ToDouble(126))
+    {
+        Console.WriteLine("Diabetes Mellitus na Gravidez");
+    }
+}
